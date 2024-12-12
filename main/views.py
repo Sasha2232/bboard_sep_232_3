1d699faca73f8a6d0cc73814871666f97313b9ca
Тен Александр Сергеевич <171016268@step.kz>
2024-12-12 21:36:11 +0500

    lesson_48

--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.exceptions import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView, CreateView
+
+from main.forms import ProfileEditForm, RegisterForm
+from main.models import AdvUser
 
 
 def index(request):
@@ -23,5 +31,44 @@ class BBLoginView(LoginView):
 
 
 @login_required
-def profile(reques):
-    return render(reques, 'main/profile.html')
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LogoutView):
+    pass
+
+
+class ProfileEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/profile_edit.html'
+    form_class = ProfileEditForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя обновлены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class PasswordEditView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_edit.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
+
+
+class RegisterView(CreateView):
+    model = AdvUser
+    template_name = 'main/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
